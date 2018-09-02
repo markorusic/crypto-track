bc994823233f459be9f3609a6585f22c57c74834
Marko Rusic <markousp5@gmail.com>
2018-09-02 02:29:32 +0200

    currency details page layout update

--- src/components/currency/CurrencyDetailsContent/index.js
+++ src/components/currency/CurrencyDetailsContent/index.js
@@ -9,47 +9,92 @@ import CurrencyPercent from 'components/currency/CurrencyPercent'
 
 const CurrencyDetailsContent = ({ currency }) => (
   <div>
-    <h1>{currency.name}({currency.symbol}) #{currency.rank}</h1>
-    <p>Last Updated: {formatDateFromNow(currency.last_updated * 1000)}</p>
-    <div className="currency-details">
-      <p>Value: {formatCurrencyValue(currency.quotes.USD.price)}</p>
-      <p>
-        Last 1h: <CurrencyPercent percent={currency.quotes.USD.percent_change_1h} />
-      </p>
-      <p>
-        Last 24h: <CurrencyPercent percent={currency.quotes.USD.percent_change_24h} />
-      </p>
-      <p>
-        Last 7d: <CurrencyPercent percent={currency.quotes.USD.percent_change_7d} />
-      </p>
-      <p>
-        <LargeNumber
-          textBefore="Market Cap: $"
-          textAfter="USD"
-          number={currency.quotes.USD.market_cap}
-        />
-      </p>
-      <p>
-        <LargeNumber
-          textBefore="Circulating Supply:"
-          textAfter={currency.symbol}
-          number={currency.circulating_supply}
-        />
-      </p>
-      <p>
-        <LargeNumber
-          textBefore="Max Supply:"
-          textAfter={currency.symbol}
-          number={currency.max_supply}
-        />
-      </p>
-      <p>
-        <LargeNumber
-          textBefore="Total Supply:"
-          textAfter={currency.symbol}
-          number={currency.total_supply}
-        />
-      </p>
+    <div className="currency-details-header flex-sp-between">
+      <div>
+        <h1 className="my-10">
+          <span>{currency.name}</span>
+          <span className="short-name">({currency.symbol})</span>
+        </h1>
+        <span className="rank">Rank #{currency.rank}</span>
+      </div>
+      <div className="mt-10">
+        <table className="table-min mt-10">
+          <thead>
+            <tr>
+              <th>Last 1h</th>
+              <th>Last 24h</th>
+              <th>Last 7d</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <CurrencyPercent percent={currency.quotes.USD.percent_change_1h} />
+              </td>
+              <td>
+                <CurrencyPercent percent={currency.quotes.USD.percent_change_24h} />
+              </td>
+              <td>
+                <CurrencyPercent percent={currency.quotes.USD.percent_change_7d} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div></div><div></div><div></div>
+    </div>
+    <div className="my-10">
+      <h2>
+        <span className="small">USD Value: </span> <span>{formatCurrencyValue(currency.quotes.USD.price)}</span>
+      </h2>
+    </div>
+    <table className="table my-10">
+      <thead>
+        <tr>
+          <th>Market Cap</th>
+          <th>Volume (24h)</th>
+          <th>Circulating Supply</th>
+          <th>Max Supply</th>
+          <th>Total Supply</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <LargeNumber
+              textAfter="USD"
+              number={currency.quotes.USD.market_cap}
+            />
+          </td>
+          <td>
+            <LargeNumber
+              textAfter="USD"
+              number={currency.quotes.USD.volume_24h}
+            />
+          </td>
+          <td>
+            <LargeNumber
+              textAfter={currency.symbol}
+              number={currency.circulating_supply}
+            />
+          </td>
+          <td>
+            <LargeNumber
+              textAfter={currency.symbol}
+              number={currency.max_supply}
+            />
+          </td>
+          <td>
+            <LargeNumber
+              textAfter={currency.symbol}
+              number={currency.total_supply}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      <span>Last Updated: {formatDateFromNow(currency.last_updated * 1000)}</span>
     </div>
   </div>
 )
